de9708b9eaaeb066d34c20a4085a11ab925edb7b
Haziq Danial <haziqdanial031@gmail.com>
2022-01-16 23:29:42 +0800

    updated dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,6 @@ class HomeController extends Controller
 
     public function index()
     {
-        return redirect()->route('book-parking.index');
+        return redirect()->route('dashboard.index');
     }
 }
